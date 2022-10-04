18389b36b9476679a4e5778d6651357c7f981752
ZieselMarcel <zima0125@hss-wi.de>
2022-10-04 11:09:21 +0200

    Version 2.0

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return('<h1>Hallo Welt!!!</h1>')
+    return render_template('index.html')
+
+@app.route('/comments')
+def comments ():
+    return render_template('comments.html', comments=['Erster', 'Zweiter','Dritter'])
 
 app.run(debug=True, port=9876)
